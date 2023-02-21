47c9490da7256baa62c72e640cd70f9ec701e3ae
VSBernhard <125886388+VSBernhard@users.noreply.github.com>
2023-02-21 08:34:10 +0100

    Add files via upload

---
+++ DEploy_Templates.ps1
@@ -0,0 +1,6 @@
+﻿$MdtBuildShare = "D:\CaptureShare"
+$MdtDepShare = "D:\DeploymentShare"
+
+Invoke-WebRequest -uri "https://raw.githubusercontent.com/Digressive/MDT-Files/master/MDT-Templates/Client-Build-Template.xml" -Outfile "$MdtBuildShare\Templates\Client-Build-Template.xml"
+
+Invoke-WebRequest -uri "https://raw.githubusercontent.com/Digressive/MDT-Files/master/MDT-Templates/Client-Deploy-Template.xml" -Outfile "$MdtDepShare\Templates\Client-Deploy-Template.xml"
